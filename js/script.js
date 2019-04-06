ed6cfd18a00a01ac986397607bbd3e548dddbeaa
unknown <daniil.konovaloff@mail.ru>
2019-04-06 14:04:21 +0300

    Это наш первый комит

---
+++ js/script.js
@@ -0,0 +1,40 @@
+'use strict';
+
+
+let something;
+console.log(something);
+
+let person = {
+    name: "John",
+    age: 25,
+    isMarried: false
+};
+
+console.log(person["name"]);
+
+let arr = ["plum.png","apple.png","arrow.jpg"];
+
+console.log(arr[2]);
+
+// let answer = +prompt("Есть ли вым 18 лут?", "Да");
+
+// let answer = confirm("Are yor here?");
+
+// console.log(typeof(answer));
+
+// console.log(5 + +" - object");
+
+let incr = 10,
+    decr = 10;
+
+console.log(incr++);
+console.log(decr--);
+
+console.log(5%2);
+console.log("2" === 2);
+
+let isChecked = true,
+    isClose = false;
+
+console.log(isChecked || !isClose);
+
